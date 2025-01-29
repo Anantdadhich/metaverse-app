0ad15d0f9bc8b0ae9c2542f08416a8342f2ac5b8
Anantdadhich <anantdadhich66@gmail.com>
2025-01-29 23:41:14 +0530

    updt

--- apps/backend/src/router/v1/index.ts
+++ apps/backend/src/router/v1/index.ts
@@ -2,9 +2,11 @@ import { Router } from "express";
 import { userrouter } from "./user";
 import { spacerouter } from "./space";
 import { adminrouter } from "./admin";
-import { signupschema } from "../../types/types";
-import { hash } from "../../scryptalgo";
-
+import { signinschema, signupschema } from "../../types/types";
+import { compare, hash } from "../../scryptalgo";
+import jwt from "jsonwebtoken"
+import client from "@repo/db"
+import { JWT_PASSWORD } from "../../config";
 
   
 export const router=Router();
@@ -23,9 +25,19 @@ const hashedpadssword=await hash(parseddata.data.password)
 
 
   try {
-   
+   const user =await client.user.create({
+    data:{
+      email:parseddata.data.username,
+      password:hashedpadssword, 
+      role:parseddata.data.type === "admin" ? "User" :"Admin"
+    }
+   })
+
+   res.json({userId:user.id})
   } catch (error) {
-   
+     console.log("error ")
+      console.log(error)
+     
   }
    
    
@@ -33,8 +45,57 @@ const hashedpadssword=await hash(parseddata.data.password)
 
 });
 
-router.post("/signin",(req,res)=>{
- 
+
+
+
+
+
+
+router.post("/signin",async(req,res)=>{
+   
+  const parseddata=signinschema.safeParse(req.body);
+
+  if(!parseddata.success){
+    res.status(403).json({message:"validation failed "})
+    return 
+  } 
+
+  try {
+      const user=await client.user.findFirst({
+        where:{
+          email:parseddata.data.username
+        }
+      })
+
+
+      if(!user) {
+         res.status(403).json({message:"user not found "});
+         return
+      } 
+
+      const isvalid =await compare(parseddata.data.password,user.password)
+
+      
+
+      if(!isvalid) {
+         res.status(403).json({message:"user not valid  "});
+         return
+      } 
+
+      const token=jwt.sign({
+        userId:user.id,
+        role:user.role,
+
+      },JWT_PASSWORD)
+
+
+      res.json({
+        token
+      })
+  } catch (error) {
+      res.status(400).json({messsage:"error "})
+  }
+
 
 })
 
