c0af5ac3af420bb81ef4addc9ce71df7c7be7248
Anantdadhich <anantdadhich66@gmail.com>
2025-02-11 23:49:15 +0530

    ws

--- apps/ws/src/User.ts
+++ apps/ws/src/User.ts
@@ -1,7 +1,9 @@
+import { WebSocket } from "ws";
+import jwt ,{JwtPayload}  from "jsonwebtoken"
+import { JWT_PASSWORD } from "./data";
+import client from "@repo/db/client"
 
-
-
-
+  
 function getRandomString(length:number){
     const characters="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     let result="";
@@ -26,6 +28,58 @@ export class User {
         this.x=0;
         this.y=0;
         this.ws=ws;
+        this.inithandlers()
         
+    }
+
+    inithandlers(){
+       this.ws.on("message",async(data)=>{
+          console.log(data);
+
+          const parsedData=JSON.parse(data.toString());
+           console.log(parsedData);
+
+           switch(parsedData.type){
+             case "join": 
+              const spaceId=parsedData.payload.spaceId;
+              const token=parsedData.payload.token;
+
+              const userId=(jwt.verify(token,JWT_PASSWORD)as JwtPayload).userId 
+
+              if(!userId){
+                this.ws.close()
+                return
+              }
+               
+              console.log("joiin receiverdfd 2")
+
+              
+              this.userId=userId;
+               
+              const space=await client.space.findFirst({
+                where:{
+                    id:spaceId
+                }
+              })
+
+               console.log("join recieved 3 ")
+
+               if(!space){
+                this.ws.close()
+                return
+               }
+
+               console.log("join recieved 4 ")
+
+
+               this.spaceId=spaceId;
+                 
+               
+               this.x=Math.floor(Math.random() * space?.width)
+               this.y=Math.floor(Math.random() *space?.height)
+                  
+               
+           }
+       })
     }
 }
